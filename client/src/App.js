d2b1dabc0ebcadf1e0dbd79b15d3cdf5603caa68
Yashira Perera <yashira@Yashiras-MacBook-Pro.local>
2020-01-19 18:53:28 +0530

    Adding custom components of react tot he application.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Landing />
     </div>
   );
 }
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://user-atom:atom123@ds163825.mlab.com:63825/leave-manager-db",
+    mongoURI: "mongodb:uri",
     secretOrKey: "secret" 
   };
