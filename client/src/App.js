d3d0778e0a69f159ccd0a56fb588b2e4b4d3e91f
Yashira Perera <yashira@Yashiras-MacBook-Pro.local>
2020-01-22 20:09:14 +0530

    Adding store, reducers and actions

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,22 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
